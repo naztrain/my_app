d3ae1dae6f3676e524affe226dc109f86c1989f8
Bradley Clark <bradley.clark@apt-gwr99gywy4.lan>
2024-08-09 10:11:44 -0600

    First commit

---
+++ lib/my_app/mybroadway.ex
@@ -0,0 +1,46 @@
+defmodule MyBroadway do
+  use Broadway
+
+  alias Broadway.Message
+
+  def start_link(_opts) do
+    Broadway.start_link(__MODULE__,
+      name: __MODULE__,
+      producer: [
+        module:
+          {BroadwayKafka.Producer,
+           [
+             hosts: [localhost: 9092],
+             group_id: "group_1",
+             topics: ["homework_topic"]
+           ]},
+        concurrency: 1
+      ],
+      processors: [
+        default: [
+          concurrency: 10
+        ]
+      ],
+      batchers: [
+        default: [
+          batch_size: 100,
+          batch_timeout: 200,
+          concurrency: 10
+        ]
+      ]
+    )
+  end
+
+  @impl true
+  def handle_message(_, message, _) do
+    message
+    |> Message.update_data(fn data -> {data, String.to_integer(data) * 2} end)
+  end
+
+  @impl true
+  def handle_batch(_, messages, _, _) do
+    list = messages |> Enum.map(fn e -> e.data end)
+    IO.inspect(list, label: "Got batch")
+    messages
+  end
+end
